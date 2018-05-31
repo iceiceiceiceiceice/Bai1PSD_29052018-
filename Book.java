956210b388fdbb59112bf985ea4f4a441f03a00e
iceiceiceiceiceice <31066905+iceiceiceiceiceice@users.noreply.github.com>
2018-05-31 11:45:51 +0700

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,32 @@
+package Shop;
+
+public class Book extends Good{
+	private int pages;
+	private String author;
+	public Book(){
+		super();
+		pages = 0;
+		author = "";
+		type = "Book";
+	}
+	public Book(String id, String title, String publisher, int price, int pages, String author){
+		super(id,title,publisher,price);
+		this.pages = pages;
+		this.author = author;
+		this.type = "Book";
+	}
+	public int getPages() {
+		return pages;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setPages(int pages) {
+		this.pages = pages;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	
+	
+}
